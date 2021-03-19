c000a42f4086d6342607a490f28494f67be3fb6f
Anderson Sprenger <anderson.sprenger@edu.pucrs.br>
2021-03-18 21:13:57 -0300

    more code

--- reminder/models.swift
+++ reminder/models.swift
@@ -2,7 +2,54 @@
 //  models.swift
 //  reminder
 //
-//  Created by Anderson Sprenger on 18/03/21.
+//  Created by Anderson Sprenger, Hojin Ryu on 18/03/21.
 //
 
 import Foundation
+
+enum Priority {
+    case low
+    case medium
+    case high
+}
+
+// MARK: --What do you think of this enum?
+enum Repeat {
+    case not
+    case day
+    case week
+    case month
+    case year
+}
+
+struct Reminder {
+    var title: String
+    var notes: String
+    var date: Date?
+    var priority: Priority
+    //TODO: var repeat: Repeat
+    //TODO: var location: String?
+    
+    func setDate(_: String) -> Bool {
+        //TODO: a method that receives a String and converts it to date, than it sets self.date to it.
+        return true
+    }
+    
+    func toString() -> String {
+        return title //TODO: improve this...
+    }
+}
+
+class List {
+    var title: String
+    var reminders: Array<Reminder>
+    
+    init(title: String) {
+        self.title = title
+        reminders = Array<Reminder>()
+    }
+    
+    func toString() -> String {
+        return title
+    }
+}
